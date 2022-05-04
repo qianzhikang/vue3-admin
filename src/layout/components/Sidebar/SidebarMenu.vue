a18fea127d4b5990dff8f348a7b6accb6db27a64
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-04 19:43:14 +0800

    feat: 路由表中添加私有路由，安装path-browserify，用于对路由进行解析处理

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- 一级 menu 菜单 -->
+  <el-menu
+    :uniqueOpened="true"
+    default-active="2"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <!-- 子集 menu 菜单 -->
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+    </el-submenu>
+    <!-- 具体菜单项 -->
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <template #title>导航四</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+<script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { filterRouters, generateMenus } from '@/utils/route'
+
+const router = useRouter()
+const routes = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateMenus(filterRoutes)
+})
+console.log(JSON.stringify(routes.value))
+</script>
