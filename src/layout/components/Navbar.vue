55bcd5464c2e5ac87dcb3eb3dbf9361e9cc51751
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-04 20:22:48 +0800

    feat: 渲染基本的面包屑组件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar">
     <hamburger class="hamburger-container" />
+    <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -27,6 +28,7 @@
 <script setup>
 import { useStore } from 'vuex'
 import Hamburger from '@/components/Hamburger'
+import Breadcrumb from '@/components/Breadcrumb'
 const store = useStore()
 const logout = () => {
   store.dispatch('user/logout')
@@ -40,6 +42,9 @@ const logout = () => {
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  .breadcrumb-container {
+    float: left;
+  }
   .hamburger-container {
     line-height: 46px;
     height: 100%;
