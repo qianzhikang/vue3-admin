c2434d6c07c61b6e03b2d4c63e28582f2a8403d9
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-04 19:31:31 +0800

    feat: 处理用户被动退出的被动处理

--- src/constant/index.js
+++ src/constant/index.js
@@ -2,6 +2,6 @@ export const TOKEN = 'token'
 // token 时间戳
 export const TIME_STAMP = 'timeStamp'
 // 超时时长(毫秒) 两小时
-export const TOKEN_TIMEOUT_VALUE = 2 * 3600 * 1000
+// export const TOKEN_TIMEOUT_VALUE = 2 * 3600 * 1000
 // 调试，设置10秒，观察效果
-// export const TOKEN_TIMEOUT_VALUE = 10000
+export const TOKEN_TIMEOUT_VALUE = 10000
