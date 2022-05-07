79ebabf04af61aaf2c107a6ff9863612b6fc3eab
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-07 11:27:51 +0800

    feat: 作者模块完成

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,4 +1,50 @@
 <template>
-  <h2>Author</h2>
+  <div class="author-container">
+    <div class="header">
+      <my-avatar
+        image="https://pic-go.oss-cn-shanghai.aliyuncs.com/avatars/avatar04.jpeg"
+        height="60px"
+        width="60px"
+        :hoverable="false"
+      >
+        {{ $t('msg.profile.name') }}
+      </my-avatar>
+      <div class="header-desc">
+        <h3>{{ $t('msg.profile.name') }}</h3>
+        <span>{{ $t('msg.profile.job') }}</span>
+      </div>
+    </div>
+    <div class="info">
+      {{ $t('msg.profile.Introduction') }}
+    </div>
+  </div>
 </template>
-<script setup></script>
+
+<script setup>
+import MyAvatar from '@/components/MyAvatar/index.vue'
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
