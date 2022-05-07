dd0c3f0813c4273e69ffffcdf93736b90e34caab
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-07 10:38:17 +0800

    feat(个人中心): 头像组件

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,4 +1,14 @@
 <template>
-  <h2>Author</h2>
+  <div class="">
+    <my-avatar :image="$store.getters.userInfo.avatar" :height="'100px'" :width="'100px'" :hoverable="false">
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </my-avatar>
+  </div>
 </template>
-<script setup></script>
+
+<script setup>
+import MyAvatar from '@/components/MyAvatar'
+</script>
+
+<style lang="scss" scoped></style>
