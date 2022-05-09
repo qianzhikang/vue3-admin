98106e60e9eceeeb8aea6acef9e5ee003f48e59b
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-09 14:40:47 +0800

    feat: 文章删除

--- src/views/article-ranking/index.vue
+++ src/views/article-ranking/index.vue
@@ -18,7 +18,9 @@
           </template>
           <template #default="{ row }" v-else-if="item.prop === 'action'">
             <el-button type="primary" size="mini" @click="onShowClick(row)">{{ $t('msg.article.show') }}</el-button>
-            <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{ $t('msg.article.remove') }}</el-button>
+            <el-button type="danger" size="mini" @click="onRemoveClick(row)">
+              {{ $t('msg.article.remove') }}
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -41,9 +43,11 @@
 <script setup>
 import { ref, onActivated, onMounted } from 'vue'
 import { tableRef, initSortable } from './sortable'
-import { getArticleList } from '@/api/article'
+import { getArticleList, deleteArticle } from '@/api/article'
 import { watchSwitchLang } from '@/utils/i18n'
+import { useI18n } from 'vue-i18n'
 import { dynamicData, selectDynamicLabel, tableColumns } from './dynamic'
+import { ElMessageBox, ElMessage } from 'element-plus'
 // 表格拖拽相关
 onMounted(() => {
   initSortable(tableData, getListData)
@@ -78,6 +82,18 @@ const getListData = async () => {
   })
   tableData.value = result.list
   total.value = result.total
+}
+// 删除用户
+const i18n = useI18n()
+const onRemoveClick = (row) => {
+  ElMessageBox.confirm(i18n.t('msg.article.dialogTitle1') + row.title + i18n.t('msg.article.dialogTitle2'), {
+    type: 'warning'
+  }).then(async () => {
+    await deleteArticle(row._id)
+    ElMessage.success(i18n.t('msg.article.removeSuccess'))
+    // 重新渲染数据
+    getListData()
+  })
 }
 getListData()
 // 监听语言切换
