c9c60f494085c20f8981388f60fd1a649bc86636
qianzhikang <qzkqianzhikang@gmail.com>
2022-05-09 14:13:59 +0800

    feat: 文章排名数据渲染

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs'
-
+import rt from 'dayjs/plugin/relativeTime'
+// 语言包
+import 'dayjs/locale/zh-cn'
+import store from '@/store'
 const dateFilter = (val, format = 'YYYY-MM-DD') => {
   if (!isNaN(val)) {
     val = parseInt(val)
@@ -7,9 +10,20 @@ const dateFilter = (val, format = 'YYYY-MM-DD') => {
 
   return dayjs(val).format(format)
 }
+// 加载相对时间插件
+dayjs.extend(rt)
+function relativeTime(val) {
+  if (!isNaN(val)) {
+    val = parseInt(val)
+  }
+  return dayjs()
+    .locale(store.getters.language === 'zh' ? 'zh-cn' : 'en')
+    .to(dayjs(val))
+}
 
 export default (app) => {
   app.config.globalProperties.$filters = {
-    dateFilter
+    dateFilter,
+    relativeTime
   }
 }
